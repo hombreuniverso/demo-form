5a4414707d0ad62ff49fdddc186d45332ff545ce
Colin Karr <67452254+hombreuniverso@users.noreply.github.com>
2025-02-24 19:42:50 -0400

    Solve "errors" not defined
    Solve errors not defined by (1) testing for
    errors in the errors.ejs file code:
    if(typeof errors !== "undefined" && errors)...
    
    and (2) defining errors in the rending of
    createUserForm in the createUsersGet function:
    errors: []
    Note that each solution can work independently on
    its own but I implemented both of them simultaneously

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -10,14 +10,15 @@ exports.usersListGet = (req, res) => {
 };
 
 exports.usersCreateGet = (req, res) => {
-  res.render("createUserForm", { title: "Create User" });
+  res.render("createUserForm", { title: "Create User", errors: [] },);
 };
-
+/*
 exports.usersCreatePost = (req, res) => {
   const { firstName, lastName } = req.body;
   usersStorage.addUser({ firstName, lastName });
   res.redirect("/");
 };
+*/
 
 //Validation and Sanitization
 const { body, validationResult } = require("express-validator");
@@ -31,27 +32,37 @@ const validateUser = [
     .isAlpha()
     .withMessage(`First name ${alphaErr}`)
     .isLength({ min: 1, max: 10 })
-    .withMessage(`First name ${lengthErr}`), body("lastName")
+    .withMessage(`First name ${lengthErr}`),
+  body("lastName")
     .trim()
-    .isAlpha().withMessage(`Last name ${alphaErr}`)
+    .isAlpha()
+    .withMessage(`Last name ${alphaErr}`)
     .isLength({ min: 1, max: 10 })
     .withMessage(`Last name ${lengthErr}`),
-   
 ];
 
 //Array of midlleware validations to controller
 exports.usersCreatePost = [
   validateUser,
   (req, res) => {
+    //Improve error handling by using try-catch blocks
+    try {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).render("createUserForm", {
         title: "Create User",
         errors: errors.array(),
       });
-    } 
-    const { firstName, lastName } = req.body;
-      usersStorage.addUser({ firstName, lastName });
-      res.redirect("/");
     }
+    const { firstName, lastName } = req.body;
+    usersStorage.addUser({ firstName, lastName });
+    res.redirect("/");
+  }catch (error) {
+    console.log(error);
+    res.status(500).render("error", {
+      title: "Error",
+      message: "An error occurred while creating the user.",
+    });
+  }
+  },
 ];
